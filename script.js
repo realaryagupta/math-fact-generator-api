06eca6ee65330ccf1c7a3cb3cde611cfd5714137
aryagupta <aryagupta2108.aag@gmail.com>
2025-05-06 08:23:52 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const factBox = document.getElementById('fact');
+const button = document.getElementById('newFactBtn');
+
+async function fetchMathFact() {
+  try {
+    const res = await fetch('http://numbersapi.com/random/math');
+    const fact = await res.text();
+    factBox.textContent = fact;
+  } catch (err) {
+    factBox.textContent = 'Oops! Could not fetch a fact. Try again.';
+    console.error(err);
+  }
+}
+
+window.onload = fetchMathFact;
+button.addEventListener('click', fetchMathFact);
